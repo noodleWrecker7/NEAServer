b4085331a1edcc72b66f7479e8e5a134b4f28ba3
noodleWrecker7 <adam@adamhodgkinson.dev>
2022-04-29 18:41:47 +0100

    probably just comments

--- src/main/java/dev/adamhodgkinson/Server.java
+++ src/main/java/dev/adamhodgkinson/Server.java
@@ -8,17 +8,16 @@ public class Server {
     static SparkServer sparkServer;
 
     public static void main(String[] args) {
+        // Gets mongodb login from passed arguments
         String username = args[0];
         String password = args[1];
 
         mongoDB = new MongoDB(username, password);
         sqliteDB = new SQLiteDB("sample.db");
         sparkServer = new SparkServer(mongoDB, sqliteDB);
-
-
     }
 
-
+    /**Closes all open processes*/
     public static void close() {
         sparkServer.close();
         mongoDB.close();
